a136410433d214a9ccaade9ad888b51d772a364b
wizkid-07 <50694990+wizkid-07@users.noreply.github.com>
2020-04-03 16:38:23 +0530

    heroku 5

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ bot.action("TODAY", async ctx => {
   const date = new Date();
   const currenDay = date.toDateString().substring(0, 3);
 
-  //  ctx.reply("Today's Menu:");
+   ctx.reply("Today's Menu:");
   if (currenDay == "Mon") {
     await ctx.reply(
       "Breakfast : MonBreakfast \n \nLunch : MonLunch \n \nDinner : MonDinner "
