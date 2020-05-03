a617200de01158c7272891711cbeaac0cc249d6e
wizkid-07 <50694990+wizkid-07@users.noreply.github.com>
2020-05-03 17:55:26 +0530

    today tomm menu function

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const session = require("telegraf/session"); // To create sessions
 const Keyboard = require("telegraf-keyboard"); //telegraf keyboard package
 const fetch = require("node-fetch"); // To send requests to graphql
 const Markup = require("telegraf/markup"); // For inline keyboard
-const dotenv = require("dotenv");
-
-dotenv.config();
 
 //import scenes
 const scene = require("./registration");
@@ -18,22 +15,19 @@ const scene5 = require("./myAccount");
 const scene6 = require("./subscription");
 const scene7 = require("./myPlan");
 const scene8 = require("./addons");
-const scene9 = require('./viewOrder');
-
+const scene9 = require("./viewOrder");
 
 // intialize app with bot token
-const URL = process.env.URL; 
-const BOT_TOKEN = process.env.BOT_TOKEN ;
+const URL = process.env.URL;
+const BOT_TOKEN = process.env.BOT_TOKEN;
 const PORT = process.env.PORT || 5000;
 const bot = new Telegraf(process.env.BOT_TOKEN);
-
-// const bot = new Telegraf("1042039654:AAF6QoQG5Ten7_CMmGxSPymbh2FeLOfNSmk");
 
 // Handler For /start command
 // ctx is context object.Holds many properties about curent chat
 // reply method to send message from bot
 
-bot.start(async ctx => {
+bot.start(async (ctx) => {
   //get current chat ID
   const chatId = ctx.from.id;
 
@@ -47,7 +41,7 @@ bot.start(async ctx => {
             name
           }
         }
-        `
+        `,
   };
 
   //Send request to graphql api about current user
@@ -56,25 +50,27 @@ bot.start(async ctx => {
     method: "POST",
     body: JSON.stringify(userExists),
     headers: {
-      "Content-Type": "application/json"
-    }
+      "Content-Type": "application/json",
+    },
   })
-    .then(res => {
+    .then((res) => {
       if (res.status !== 200 && res.status !== 201) {
         throw new Error("Failed!");
       }
       return res.json();
     })
-    .then(response => {
+    .then((response) => {
       //If user is already in database . Show order Button
       if (response.data.userexists !== null) {
         const keyboard = new Keyboard();
         keyboard
           .add("Wallet", "Menu")
           .add("Subscribe Plans", "Order Meals", "Order Addons")
-          .add("My Plans", "My Account","My Orders");
+          .add("My Plans", "My Account", "My Orders");
         ctx.reply(
-          "Hi " + response.data.userexists.name + "! .How can we help you order today?",
+          "Hi " +
+            response.data.userexists.name +
+            "! .How can we help you order today?",
           keyboard.draw()
         );
       }
@@ -84,18 +80,13 @@ bot.start(async ctx => {
         ctx.reply(
           "Lets get started ! Click the below button and match the follow ups!",
           Markup.inlineKeyboard([
-            Markup.callbackButton("Register", "REGISTER_NOW")
+            Markup.callbackButton("Register", "REGISTER_NOW"),
           ]).extra()
         );
       }
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 });
-
-
-
-
-
 
 // Intialize stage object
 // Stages and scenes are used for dialogue between bot and user
@@ -119,122 +110,36 @@ bot.use(session());
 bot.use(stage.middleware());
 
 // Callback for inline register button .Enter into Regsiter scene
-bot.action("REGISTER_NOW", ctx => ctx.scene.enter("Register"));
+bot.action("REGISTER_NOW", (ctx) => ctx.scene.enter("Register"));
 
 // Callback for order button .Enter into order scene
-bot.hears("Order Meals", ctx => ctx.scene.enter("OrderScene"));
+bot.hears("Order Meals", (ctx) => ctx.scene.enter("OrderScene"));
 
-bot.hears("My Orders", ctx => ctx.scene.enter("ViewOrderScene"));
+bot.hears("My Orders", (ctx) => ctx.scene.enter("ViewOrderScene"));
 
-bot.hears("Menu", ctx => ctx.scene.enter("MenuScene"));
+bot.hears("Menu", (ctx) => ctx.scene.enter("MenuScene"));
 
+bot.hears("Wallet", (ctx) => ctx.scene.enter("WalletScene"));
 
+bot.action("RECHARGE_NOW", (ctx) => ctx.scene.enter("WalletScene"));
 
-bot.hears("Wallet", ctx => ctx.scene.enter("WalletScene"));
+bot.hears("My Account", (ctx) => ctx.scene.enter("AccountScene"));
 
-bot.action("RECHARGE_NOW", ctx => ctx.scene.enter("WalletScene"));
+bot.hears("Subscribe Plans", (ctx) => ctx.scene.enter("SubscriptionScene"));
 
-bot.hears("My Account", ctx => ctx.scene.enter("AccountScene"));
+bot.hears("My Plans", (ctx) => ctx.scene.enter("MyPlanScene"));
 
-bot.hears("Subscribe Plans", ctx => ctx.scene.enter("SubscriptionScene"));
+bot.hears("Order Addons", (ctx) => ctx.scene.enter("AddonScene"));
 
-bot.hears("My Plans", ctx => ctx.scene.enter("MyPlanScene"));
+bot.action("SUBSCRIBE_NOW", (ctx) => ctx.scene.enter("SubscriptionScene"));
 
-bot.hears("Order Addons", ctx => ctx.scene.enter("AddonScene"));
-
-bot.action("SUBSCRIBE_NOW", ctx => ctx.scene.enter("SubscriptionScene"));
-
-
-bot.action("TODAY", async ctx => {
-  const date = new Date();
-  const currenDay = date.toDateString().substring(0, 3);
-
-  
-  if (currenDay == "Mon") {
-   
-    await ctx.replyWithMarkdown(
-      "T O D A Y' S M E N U \n\n*NORTH INDIAN* \n\n_Breakfast_ \nDosa with chutney,vada & sambar\n\n_Lunch_ \nWhite rice with moong dhaal curry,seasonal vegetable & paapad\n\nDinner : \nPoori with potato mash\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nDosa with chutney,vada & sambar\n\n_Lunch_ \nWhite rice with pachapayir sambar,seasonal vegetable & paapad\n\n_Dinner_ \nPoori with potato mash"
-    );
-  }
-  if (currenDay == "Tue") {
-    await ctx.replyWithMarkdown(
-      "T O D A Y' S M E N U \n\n*NORTH INDIAN*  \n\n_Breakfast_ \nPoori with potato mash\n\n_Lunch_ \nWhite rice with toor dhaal tadka,seasonal vegetable & paapad\n\n_Dinner_ \nPlain paratha with chole masala\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nPoori with potato mash\n\n_Lunch_ \nWhite rice with sambar,seasonal vegetable & paapad\n\n_Dinner_ \nMasala dosa with chutney "
-    );
-  }
-  if (currenDay == "Wed") {
-    await ctx.replyWithMarkdown(
-      "T O D A Y' S M E N U \n\n*NORTH INDIAN*  \n\n_Breakfast_ \nIdly with chutney,vada and sambar\n\n_Lunch_ \nWhite rice with rajma curry,seasonal vegetable & paapad\n\n_Dinner_ \nPhulka with panner butter masala\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nIdly with chutney and sambar\n\n_Lunch_ \nWhite rice with vathal kulambu,seasonal vegetable & paapad\n\n_Dinner_ \nIdly with sambar"
-    );
-  }
-  if (currenDay == "Thu") {
-    await ctx.replyWithMarkdown(
-      "T O D A Y' S M E N U \n\n*NORTH INDIAN*  \n\n_Breakfast_ \nAloo paratha with curd & pickle\n\n_Lunch_ \nWhite rice with khadi,seasonal vegetable & paapad\n\n_Dinner_ \nChole batura\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nAloo paratha with curd & pickle\n\n_Lunch_ \nWhite rice with more kolambu,seasonal vegetable & paapad\n\n_Dinner_ \nchole batura"
-    );
-  }
-  if (currenDay == "Fri") {
-    await ctx.replyWithMarkdown(
-      "T O D A Y' S M E N U \n\n*NORTH INDIAN*  \n\n_Breakfast_ \nMasala Dosa with chutney\n\n_Lunch_ \nWhite rice with mysore dhaal tadka,seasonal vegetable & paapad\n\n_Dinner_ \nAloo paratha with curd & pickle\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nMasala Dosa with chutney\n\n_Lunch_ \nWhite rice with mysore dhaal curry,seasonal vegetable & paapad\n\n_Dinner_ \nCumin rice with gravy"
-    );
-  }
-  if (currenDay == "Sat") {
-    await ctx.replyWithMarkdown(
-      "T O D A Y' S M E N U \n\n*NORTH INDIAN*  \n\n_Breakfast_ \nDhaalpoori\n\n_Lunch_ \nWhite rice with channa dhaal,seasonal vegetable & paapad\n\n_Dinner_ \nPlain paratha with matar paneer\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nDhaalpoori\n\n_Lunch_ \nWhite rice with kadala curry,seasonal vegetable & paapad\n\n_Dinner_ \nPuttu with kadala curry"
-    );
-  }
-  if (currenDay == "Sun") {
-    await ctx.replyWithMarkdown(
-      "T O D A Y' S M E N U \n\n*NORTH INDIAN*  \n\n_Breakfast_ \nSandwich with sauce sachet\n\n_Lunch_ \nWhite rice with dhaal makhani,seasonal vegetable & paapad\n\n_Dinner_ \nChapathi with chole masala\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nSandwich with sauce sachet\n\n_Lunch_ \nWhite rice with dhaal makhani,seasonal vegetable & paapad\n\n_Dinner_ \nChapathi with chole masala"
-    );
-  }
+bot.action("TODAY", async (ctx) => {
+  scene3.displayTodayMenu(ctx);
 });
 
-bot.action("TOMORROW_MENU", async ctx => {
-  const today = new Date();
-  const tomorrow = new Date(today);
-  tomorrow.setDate(tomorrow.getDate() + 1);
-  const tomorrowDay = tomorrow.toDateString().substring(0, 3);
-  
-  if (tomorrowDay == "Mon") {
-   
-    await ctx.replyWithMarkdown(
-      "T O M O R R O W' S M E N U\n\n*NORTH INDIAN*  \n\n_Breakfast_ \nDosa with chutney,vada & sambar\n\n_Lunch_ \nWhite rice with moong dhaal curry,seasonal vegetable & paapad\n\n_Dinner_ \nPoori with potato mash\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nDosa with chutney,vada & sambar\n\n_Lunch_ \nWhite rice with pachapayir sambar,seasonal vegetable & paapad\n\n_Dinner_ \nPoori with potato mash"
-    );
-  }
-  if (tomorrowDay == "Tue") {
-    await ctx.replyWithMarkdown(
-      "T O M O R R O W' S M E N U\n\n*NORTH INDIAN*  \n\n_Breakfast_ \nPoori with potato mash\n\n_Lunch_ \nWhite rice with toor dhaal tadka,seasonal vegetable & paapad\n\n_Dinner_ \nPlain paratha with chole masala\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nPoori with potato mash\n\n_Lunch_ \nWhite rice with sambar,seasonal vegetable & paapad\n\n_Dinner_ \nMasala dosa with chutney "
-    );
-  }
-  if (tomorrowDay == "Wed") {
-    await ctx.replyWithMarkdown(
-      "T O M O R R O W' S M E N U\n\n*NORTH INDIAN*  \n\n_Breakfast_ \nIdly with chutney,vada and sambar\n\n_Lunch_ \nWhite rice with rajma curry,seasonal vegetable & paapad\n\n_Dinner_ \nPhulka with panner butter masala\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nIdly with chutney,vada and sambar\n\n_Lunch_ \nWhite rice with vathal kulambu,seasonal vegetable & paapad\n\n_Dinner_ \nIdly with sambar"
-    );
-  }
-  if (tomorrowDay == "Thu") {
-    await ctx.replyWithMarkdown(
-      "T O M O R R O W' S M E N U\n\n*NORTH INDIAN*  \n\n_Breakfast_ \nAloo paratha with curd & pickle\n\n_Lunch_ \nWhite rice with khadi,seasonal vegetable & paapad\n\n_Dinner_ \nChole batura\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nAloo paratha with curd & pickle\n\n_Lunch_ \nWhite rice with more kolambu,seasonal vegetable & paapad\n\n_Dinner_ \nchole batura"
-    );
-  }
-  if (tomorrowDay == "Fri") {
-    await ctx.replyWithMarkdown(
-      "T O M O R R O W' S M E N U\n\n*NORTH INDIAN*  \n\n_Breakfast_ \nMasala Dosa with chutney\n\n_Lunch_ \nWhite rice with mysore dhaal tadka,seasonal vegetable & paapad\n\n_Dinner_ \nAloo paratha with curd & pickle\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nMasala Dosa with chutney\n\n_Lunch_ \nWhite rice with mysore dhaal curry,seasonal vegetable & paapad\n\n_Dinner_ \nCumin rice with gravy"
-    );
-  }
-  if (tomorrowDay == "Sat") {
-    await ctx.replyWithMarkdown(
-      "T O M O R R O W' S M E N U\n\n*NORTH INDIAN*  \n\n_Breakfast_ \nDhaalpoori\n\n_Lunch_ \nWhite rice with channa dhaal,seasonal vegetable & paapad\n\n_Dinner_ \nPlain paratha with matar paneer\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nDhaalpoori\n\n_Lunch_ \nWhite rice with kadala curry,seasonal vegetable & paapad\n\n_Dinner_ \nPuttu with kadala curry"
-    );
-  }
-  if (tomorrowDay == "Sun") {
-    await ctx.replyWithMarkdown(
-      "T O M O R R O W' S M E N U\n\n*NORTH INDIAN*  \n\n_Breakfast_ \nSandwich with sauce sachet\n\n_Lunch_ \nWhite rice with dhaal makhani,seasonal vegetable & paapad\n\n_Dinner_ \nChapathi with chole masala\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nSandwich with sauce sachet\n\n_Lunch_ \nWhite rice with dhaal makhani,seasonal vegetable & paapad\n\n_Dinner_ \nChapathi with chole masala"
-    );
-  }
+bot.action("TOMORROW_MENU", async (ctx) => {
+  scene3.displayTommorrowMenu(ctx);
 });
-
-
-
-
 
 bot.telegram.setWebhook(`${URL}bot${BOT_TOKEN}`);
 bot.startWebhook(`/bot${BOT_TOKEN}`, null, PORT);
--- menu.js
+++ menu.js
@@ -1,86 +1,157 @@
-const WizardScene=require('telegraf/scenes/wizard');
-const Keyboard = require('telegraf-keyboard');
-const Markup = require('telegraf/markup');
-const Extra = require('telegraf/extra')
-const fetch = require('node-fetch');
-// const Telegraf =require('telegraf');
-// const bot =new Telegraf("1042039654:AAF6QoQG5Ten7_CMmGxSPymbh2FeLOfNSmk");
+const WizardScene = require("telegraf/scenes/wizard");
+const Markup = require("telegraf/markup");
+const fetch = require("node-fetch");
 
 
-const menuScene=new WizardScene(
-    'MenuScene',
-    async ctx=>{
+const menuScene = new WizardScene("MenuScene", async (ctx) => {
+  let notRegistered = false;
 
-
-        let notRegistered=false;
-
-    const userExists = {
-      query: `
+  const userExists = {
+    query: `
           query
           {
             userexists(chatId:"${ctx.from.id}"){
               name
             }
           }
-          `
-    };
-  
-    //Send request to graphql api about current user
-  
-    await fetch("https://metrono-backend.herokuapp.com/graphql", {
-      method: "POST",
-      body: JSON.stringify(userExists),
-      headers: {
-        "Content-Type": "application/json"
+          `,
+  };
+
+  //Send request to graphql api about current user
+
+  await fetch("https://metrono-backend.herokuapp.com/graphql", {
+    method: "POST",
+    body: JSON.stringify(userExists),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => {
+      if (res.status !== 200 && res.status !== 201) {
+        throw new Error("Failed!");
+      }
+      return res.json();
+    })
+    .then((response) => {
+      //If user is already in database . Show order Button
+      if (!response.data.userexists) {
+        notRegistered = true;
       }
     })
-      .then(res => {
-        if (res.status !== 200 && res.status !== 201) {
-          throw new Error("Failed!");
-        }
-        return res.json();
-      })
-      .then(response => {
-        //If user is already in database . Show order Button
-        if (!response.data.userexists) {
-         notRegistered=true;
-        }
-  
-       
-      })
-      .catch(err => console.log(err));
-  
+    .catch((err) => console.log(err));
 
-  if (notRegistered)
-  {
+  if (notRegistered) {
     ctx.reply(
       "Sorry you have to register first!",
       Markup.inlineKeyboard([
-        Markup.callbackButton("Register", "REGISTER_NOW")
+        Markup.callbackButton("Register", "REGISTER_NOW"),
       ]).extra()
     );
 
     return ctx.scene.leave();
   }
 
-       
-        ctx.reply('See the menu for ...',Markup.inlineKeyboard([
-            [Markup.callbackButton('Today', 'TODAY'),
-            Markup.callbackButton('Tomorrow', 'TOMORROW_MENU')],[Markup.urlButton('Download Menu','https://drive.google.com/file/d/1YMOpnNcStMSZX5HsbsL92pyCZc8yTMyH/view?usp=sharing')
-        ]
-          ]).extra());
+  ctx.reply(
+    "See the menu for ...",
+    Markup.inlineKeyboard([
+      [
+        Markup.callbackButton("Today", "TODAY"),
+        Markup.callbackButton("Tomorrow", "TOMORROW_MENU"),
+      ],
+      [
+        Markup.urlButton(
+          "Download Menu",
+          "https://drive.google.com/file/d/1YMOpnNcStMSZX5HsbsL92pyCZc8yTMyH/view?usp=sharing"
+        ),
+      ],
+    ]).extra()
+  );
 
-          
+  return ctx.scene.leave();
+});
 
-        
+exports.menuScene = menuScene;
 
-          return ctx.scene.leave();
+exports.displayTodayMenu = async function (ctx) {
+  const date = new Date();
+  const currenDay = date.toDateString().substring(0, 3);
 
+  if (currenDay == "Mon") {
+    await ctx.replyWithMarkdown(
+      "T O D A Y' S M E N U \n\n*NORTH INDIAN* \n\n_Breakfast_ \nDosa with chutney,vada & sambar\n\n_Lunch_ \nWhite rice with moong dhaal curry,seasonal vegetable & paapad\n\nDinner : \nPoori with potato mash\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nDosa with chutney,vada & sambar\n\n_Lunch_ \nWhite rice with pachapayir sambar,seasonal vegetable & paapad\n\n_Dinner_ \nPoori with potato mash"
+    );
+  }
+  if (currenDay == "Tue") {
+    await ctx.replyWithMarkdown(
+      "T O D A Y' S M E N U \n\n*NORTH INDIAN*  \n\n_Breakfast_ \nPoori with potato mash\n\n_Lunch_ \nWhite rice with toor dhaal tadka,seasonal vegetable & paapad\n\n_Dinner_ \nPlain paratha with chole masala\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nPoori with potato mash\n\n_Lunch_ \nWhite rice with sambar,seasonal vegetable & paapad\n\n_Dinner_ \nMasala dosa with chutney "
+    );
+  }
+  if (currenDay == "Wed") {
+    await ctx.replyWithMarkdown(
+      "T O D A Y' S M E N U \n\n*NORTH INDIAN*  \n\n_Breakfast_ \nIdly with chutney,vada and sambar\n\n_Lunch_ \nWhite rice with rajma curry,seasonal vegetable & paapad\n\n_Dinner_ \nPhulka with panner butter masala\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nIdly with chutney and sambar\n\n_Lunch_ \nWhite rice with vathal kulambu,seasonal vegetable & paapad\n\n_Dinner_ \nIdly with sambar"
+    );
+  }
+  if (currenDay == "Thu") {
+    await ctx.replyWithMarkdown(
+      "T O D A Y' S M E N U \n\n*NORTH INDIAN*  \n\n_Breakfast_ \nAloo paratha with curd & pickle\n\n_Lunch_ \nWhite rice with khadi,seasonal vegetable & paapad\n\n_Dinner_ \nChole batura\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nAloo paratha with curd & pickle\n\n_Lunch_ \nWhite rice with more kolambu,seasonal vegetable & paapad\n\n_Dinner_ \nchole batura"
+    );
+  }
+  if (currenDay == "Fri") {
+    await ctx.replyWithMarkdown(
+      "T O D A Y' S M E N U \n\n*NORTH INDIAN*  \n\n_Breakfast_ \nMasala Dosa with chutney\n\n_Lunch_ \nWhite rice with mysore dhaal tadka,seasonal vegetable & paapad\n\n_Dinner_ \nAloo paratha with curd & pickle\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nMasala Dosa with chutney\n\n_Lunch_ \nWhite rice with mysore dhaal curry,seasonal vegetable & paapad\n\n_Dinner_ \nCumin rice with gravy"
+    );
+  }
+  if (currenDay == "Sat") {
+    await ctx.replyWithMarkdown(
+      "T O D A Y' S M E N U \n\n*NORTH INDIAN*  \n\n_Breakfast_ \nDhaalpoori\n\n_Lunch_ \nWhite rice with channa dhaal,seasonal vegetable & paapad\n\n_Dinner_ \nPlain paratha with matar paneer\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nDhaalpoori\n\n_Lunch_ \nWhite rice with kadala curry,seasonal vegetable & paapad\n\n_Dinner_ \nPuttu with kadala curry"
+    );
+  }
+  if (currenDay == "Sun") {
+    await ctx.replyWithMarkdown(
+      "T O D A Y' S M E N U \n\n*NORTH INDIAN*  \n\n_Breakfast_ \nSandwich with sauce sachet\n\n_Lunch_ \nWhite rice with dhaal makhani,seasonal vegetable & paapad\n\n_Dinner_ \nChapathi with chole masala\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nSandwich with sauce sachet\n\n_Lunch_ \nWhite rice with dhaal makhani,seasonal vegetable & paapad\n\n_Dinner_ \nChapathi with chole masala"
+    );
+  }
+};
 
+exports.displayTommorrowMenu = async function (ctx) {
+  const today = new Date();
+  const tomorrow = new Date(today);
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  const tomorrowDay = tomorrow.toDateString().substring(0, 3);
 
-    }
-)
-
-
-
-exports.menuScene=menuScene;
+  if (tomorrowDay == "Mon") {
+    await ctx.replyWithMarkdown(
+      "T O M O R R O W' S M E N U\n\n*NORTH INDIAN*  \n\n_Breakfast_ \nDosa with chutney,vada & sambar\n\n_Lunch_ \nWhite rice with moong dhaal curry,seasonal vegetable & paapad\n\n_Dinner_ \nPoori with potato mash\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nDosa with chutney,vada & sambar\n\n_Lunch_ \nWhite rice with pachapayir sambar,seasonal vegetable & paapad\n\n_Dinner_ \nPoori with potato mash"
+    );
+  }
+  if (tomorrowDay == "Tue") {
+    await ctx.replyWithMarkdown(
+      "T O M O R R O W' S M E N U\n\n*NORTH INDIAN*  \n\n_Breakfast_ \nPoori with potato mash\n\n_Lunch_ \nWhite rice with toor dhaal tadka,seasonal vegetable & paapad\n\n_Dinner_ \nPlain paratha with chole masala\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nPoori with potato mash\n\n_Lunch_ \nWhite rice with sambar,seasonal vegetable & paapad\n\n_Dinner_ \nMasala dosa with chutney "
+    );
+  }
+  if (tomorrowDay == "Wed") {
+    await ctx.replyWithMarkdown(
+      "T O M O R R O W' S M E N U\n\n*NORTH INDIAN*  \n\n_Breakfast_ \nIdly with chutney,vada and sambar\n\n_Lunch_ \nWhite rice with rajma curry,seasonal vegetable & paapad\n\n_Dinner_ \nPhulka with panner butter masala\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nIdly with chutney,vada and sambar\n\n_Lunch_ \nWhite rice with vathal kulambu,seasonal vegetable & paapad\n\n_Dinner_ \nIdly with sambar"
+    );
+  }
+  if (tomorrowDay == "Thu") {
+    await ctx.replyWithMarkdown(
+      "T O M O R R O W' S M E N U\n\n*NORTH INDIAN*  \n\n_Breakfast_ \nAloo paratha with curd & pickle\n\n_Lunch_ \nWhite rice with khadi,seasonal vegetable & paapad\n\n_Dinner_ \nChole batura\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nAloo paratha with curd & pickle\n\n_Lunch_ \nWhite rice with more kolambu,seasonal vegetable & paapad\n\n_Dinner_ \nchole batura"
+    );
+  }
+  if (tomorrowDay == "Fri") {
+    await ctx.replyWithMarkdown(
+      "T O M O R R O W' S M E N U\n\n*NORTH INDIAN*  \n\n_Breakfast_ \nMasala Dosa with chutney\n\n_Lunch_ \nWhite rice with mysore dhaal tadka,seasonal vegetable & paapad\n\n_Dinner_ \nAloo paratha with curd & pickle\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nMasala Dosa with chutney\n\n_Lunch_ \nWhite rice with mysore dhaal curry,seasonal vegetable & paapad\n\n_Dinner_ \nCumin rice with gravy"
+    );
+  }
+  if (tomorrowDay == "Sat") {
+    await ctx.replyWithMarkdown(
+      "T O M O R R O W' S M E N U\n\n*NORTH INDIAN*  \n\n_Breakfast_ \nDhaalpoori\n\n_Lunch_ \nWhite rice with channa dhaal,seasonal vegetable & paapad\n\n_Dinner_ \nPlain paratha with matar paneer\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nDhaalpoori\n\n_Lunch_ \nWhite rice with kadala curry,seasonal vegetable & paapad\n\n_Dinner_ \nPuttu with kadala curry"
+    );
+  }
+  if (tomorrowDay == "Sun") {
+    await ctx.replyWithMarkdown(
+      "T O M O R R O W' S M E N U\n\n*NORTH INDIAN*  \n\n_Breakfast_ \nSandwich with sauce sachet\n\n_Lunch_ \nWhite rice with dhaal makhani,seasonal vegetable & paapad\n\n_Dinner_ \nChapathi with chole masala\n---------------------------------------------------------\n*SOUTH INDIAN* \n\n_Breakfast_ \nSandwich with sauce sachet\n\n_Lunch_ \nWhite rice with dhaal makhani,seasonal vegetable & paapad\n\n_Dinner_ \nChapathi with chole masala"
+    );
+  }
+};
